4bf589a24e1267df0d8836242b3b22b4abeb6d14
xx127 <xx1271328330@163.com>
2022-12-30 14:17:47 +0800

    bugfix

---
+++ 大小号接悬赏.py
@@ -0,0 +1,56 @@
+import math
+import win32gui, win32con, win32api,win32ui
+from PIL import Image
+import cv2
+import numpy as np
+import aircv as ac
+import time
+import CommonUtils
+import random
+import datetime
+import os
+import logging
+
+import async_all
+
+# 体力
+power = 350
+# 每局消耗体力
+consume = 8
+# 计划打的局数
+maxcount = 0
+# 每局消耗时间
+consumeTime = 26
+# 大等待时间
+bigWaitTime = 3
+# 小等待时间
+smallWaitTime = 2
+接受邀请 = './image/通用图片/接受邀请.png'
+hwndBig = win32gui.FindWindow(0, "铁血战士胖虎")
+hwndSmall = win32gui.FindWindow(0, "捉鼠大师小叮当")
+
+
+def 开始组队魂土() :
+    count = 0
+    print("开始监控是否有悬赏")
+    while 1 == 1 :
+        time.sleep(random.uniform(6.0, 7.0))
+        # 检查大号
+        while CommonUtils.openimages(接受邀请, hwndBig) != 0:
+            startX, startY, = CommonUtils.openimages(接受邀请, hwndBig)
+            CommonUtils.click_point_random(startX, startY, hwndBig)
+            print("---大号获取了一个悬赏---")
+            break
+        # 检查小号
+        while CommonUtils.openimages(接受邀请, hwndSmall) != 0:
+            startX, startY, = CommonUtils.openimages(接受邀请, hwndSmall)
+            CommonUtils.click_point_random(startX, startY, hwndSmall)
+            print("---小号获取了一个悬赏---")
+            break
+
+        count = count + 1
+        print("监控持续" + str(count) + "次")
+
+
+
+开始组队魂土()
