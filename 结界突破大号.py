313e1fa4fc48f4eaa0491e1ce543c5f18dad001c
xulianglikejava <xl11112222>
2023-02-20 23:50:33 +0800

    666

--- 结界突破大号.py
+++ 结界突破大号.py
@@ -128,7 +128,7 @@ def 开始结界突破() :
         # 如果最后一次 那么就要退出2次
         if i == 9 or i == 16 or  i == 27 :
             time.sleep(random.uniform(3, 3.5))
-            print('9退2')
+            print('9退4')
             for j in range(4):
                 j += 1
                 print("退出第" + str(j) + "次" )
